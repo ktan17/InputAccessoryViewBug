ee2671997b74ce5371f6c7aecf3db0acf52761e1
Kevin Tan <ktan17@g.ucla.edu>
2022-08-01 11:20:28 -0700

    Initial Commit

---
+++ help/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  help
+//
+//  Created by Kevin Tan on 8/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
