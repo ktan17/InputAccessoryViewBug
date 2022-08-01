ffbd15861046f3155be4c9c093a045466cfdf330
Kevin Tan <ktan17@g.ucla.edu>
2022-08-01 11:37:53 -0700

    add readme

--- help/ViewController.swift
+++ help/ViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  override var canBecomeFirstResponder: Bool {
+    true
+  }
+
+  override var inputAccessoryView: UIView? {
+    v
+  }
+
+  private let v: UIView = {
+    let a = UIView()
+    a.backgroundColor = .red
+    a.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+    return a
+  }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+
   }
 
 
